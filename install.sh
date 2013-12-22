9094b0ecbf08e11198429240d9ce8f6d252a2180
jfjhh <alex.striff1@gmail.com>
2013-12-22 14:20:07 -0800

    allow icon editing (chmod .app)

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ mkdir ~/Library/Application\ Support/Pomodoro\ Timer
 cp -v JavaPomodoroTimer/Pomodoro_Timer.jar ~/Library/Application\ Support/Pomodoro\ Timer
 cp -v JavaPomodoroTimer/Timer/icon/pomodoro.png ~/Library/Application\ Support/Pomodoro\ Timer
 sudo cp -Rv JavaPomodoroTimer/Pomodoro\ Timer.app /Applications/
+sudo chmod 777 /Applicatons/Pomodoro\ Timer.app
 sleep 1
 rm -rfv JavaPomodoroTimer
 echo "FINISHED INSTALL; EXITING..."
