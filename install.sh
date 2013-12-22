677a1fa1d66738783b809f677d2e847d7f96437e
jfjhh <alex.striff1@gmail.com>
2013-12-22 13:17:54 -0800

    fix app copying

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ echo "INSTALLING POMODORO TIMER..."
 git clone https://github.com/jfjhh/JavaPomodoroTimer.git
 sleep 1
 mkdir ~/Library/Application\ Support/Pomodoro\ Timer
-ditto -V JavaPomodoroTimer/Pomodoro_Timer.jar ~/Library/Application\ Support/Pomodoro\ Timer
-ditto -V JavaPomodoroTimer/Pomodoro\ Timer.app /Applications/
+cp -v JavaPomodoroTimer/Pomodoro_Timer.jar ~/Library/Application\ Support/Pomodoro\ Timer
+sudo cp -Rv JavaPomodoroTimer/Pomodoro\ Timer.app /Applications/
 sleep 1
 rm -rfv JavaPomodoroTimer
 echo "FINISHED INSTALL; EXITING..."
